1052a189d77f8c2734fa5319afcc106787e88e72
sins621 <bradlycarpenterza@gmail.com>
2025-01-30 21:05:55 +0200

    update/intermediate-js-done

---
+++ JS/Intro/intro.js
@@ -0,0 +1,15 @@
+var age = 27;
+var years_left = 90 - age;
+var days = years_left * 365;
+var weeks = years_left * 52;
+var months = years_left * 12;
+
+console.log(
+  "You have " +
+    days +
+    " days, " +
+    weeks +
+    " weeks, and " +
+    months +
+    " months left.",
+);
