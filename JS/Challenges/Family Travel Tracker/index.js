9d50f33eec90676f8f06d886af6995c8c0819c4c
sins621 <bradlycarpenterza@gmail.com>
2025-02-13 01:07:34 +0200

    update/add-family-tracker

---
+++ JS/Challenges/Family Travel Tracker/index.js
@@ -0,0 +1,84 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "123456",
+  port: 5432,
+});
+db.connect();
+
+const user_query = await db.query("SELECT * FROM users");
+let users = user_query.rows;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentUserId = 1;
+
+async function checkVisisted() {
+  const result = await db.query(
+    "SELECT * FROM visited_countries WHERE user_id = $1",
+    [currentUserId],
+  );
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+app.get("/", async (_req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: "teal",
+  });
+});
+
+app.post("/user", async (req, res) => {
+  currentUserId = req.body.user;
+  res.redirect("/");
+});
+
+//app.post("/add", async (req, res) => {
+//  const input = req.body["country"];
+//
+//  try {
+//    const result = await db.query(
+//      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+//      [input.toLowerCase()],
+//    );
+//
+//    const data = result.rows[0];
+//    const countryCode = data.country_code;
+//    try {
+//      await db.query(
+//        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+//        [countryCode],
+//      );
+//      res.redirect("/");
+//    } catch (err) {
+//      console.log(err);
+//    }
+//  } catch (err) {
+//    console.log(err);
+//  }
+//});
+
+app.post("/new", async (req, res) => {
+  //Hint: The RETURNING keyword can return the data that was inserted.
+  //https://www.postgresql.org/docs/current/dml-returning.html
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
