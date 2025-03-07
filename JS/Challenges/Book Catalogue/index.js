0b1100055adca9ffe121dc5bdfca1428f07cd1b3
Sins_621 <sinsmailza@gmail.com>
2025-03-07 18:25:40 +0200

    save/adding-db-model

--- JS/Challenges/Book Catalogue/index.js
+++ JS/Challenges/Book Catalogue/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
-import pg from "pg";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import "dotenv/config";
 import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import session from "express-session";
+import DatabaseHandler from "./models/databasehandler.js";
 
 // TODO: Improve DB Query Error Logging Messages
 
@@ -17,7 +17,7 @@ APP.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-  }),
+  })
 );
 APP.use(bodyParser.urlencoded({ extended: true }));
 APP.use(bodyParser.json());
@@ -28,15 +28,15 @@ APP.use(passport.session());
 
 const PORT = 6199;
 
-const DB = new pg.Client({
+const CLIENT_INFO = {
   user: "postgres",
   host: process.env.DB_HOST,
   database: "book_website",
   password: process.env.DB_PASS,
   port: 5432,
-});
+};
 
-DB.connect();
+const databaseHandler = new DatabaseHandler(CLIENT_INFO);
 
 const CATEGORIES = [
   "Fantasy",
@@ -73,34 +73,21 @@ function today() {
   if (mm < 10) {
     mm = "0" + mm;
   }
-
   return yyyy + "-" + mm + "-" + dd;
 }
 
 // Home
 APP.get("/", async (req, res) => {
-  console.log(req.user);
-  if (req.isAuthenticated()) {
-    var user = req.user;
-  } else {
-    var user = null;
-  }
-
   try {
-    var bookQuery = await DB.query("SELECT * FROM books");
+    var books = await databaseHandler.fetchBooks();
   } catch (err) {
     console.log(`DB Error: ${err}`);
   }
-
-  if (bookQuery.rows.length === 0)
-    return res.send("Error Retrieving Books").status(500);
-
-  const BOOKS = bookQuery.rows;
-
+  if (books.length === 0) return res.send("Error Retrieving Books").status(500);
   return res.render("index.ejs", {
     categories: CATEGORIES,
-    books: BOOKS,
-    user: user,
+    books: books,
+    user: req.user,
   });
 });
 
@@ -112,10 +99,10 @@ APP.get("/filter", async (req, res) => {
   }
 
   try {
-    var bookQuery = await DB.query(
+    var bookQuery = await databaseHandler.database.query(
       `SELECT * FROM books
        WHERE category=$1`,
-      [req.query.category],
+      [req.query.category]
     );
   } catch (err) {
     console.log(`DB Error: ${err}`);
@@ -169,29 +156,17 @@ APP.post("/submit", async (req, res) => {
 
   const BOOK = JSON.parse(req.body.book);
   try {
-    await DB.query(
-      `INSERT INTO books (
-       title,
-       author,
-       category,
-       publish_year,
-       abstract,
-       cover_id,
-       quantity,
-       price
-       )
-     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-      [
-        BOOK.title,
-        BOOK.author_name[0],
-        req.body.category,
-        BOOK.publish_year[0],
-        req.body.abstract,
-        BOOK.cover_i,
-        req.body.quantity,
-        req.body.price,
-      ],
-    );
+    const BOOK_INFO = [
+      BOOK.title,
+      BOOK.author_name[0],
+      req.body.category,
+      BOOK.publish_year[0],
+      req.body.abstract,
+      BOOK.cover_i,
+      req.body.quantity,
+      req.body.price,
+    ];
+    databaseHandler.addBook(BOOK_INFO);
   } catch (err) {
     console.log(`Server Error: ${err}`);
   }
@@ -201,43 +176,22 @@ APP.post("/submit", async (req, res) => {
 
 APP.get("/book_focus", async (req, res) => {
   if (!req.query) return res.send("Server Error").status(500);
-
-  if (req.isAuthenticated()) {
-    var user = req.user;
-  } else {
-    var user = null;
-  }
-
   const BOOK_ID = req.query.book_id;
   try {
-    var bookQuery = await DB.query("SELECT * FROM books WHERE id = $1", [
-      BOOK_ID,
-    ]);
+    var book = await databaseHandler.fetchBook({id:BOOK_ID});
   } catch (err) {
-    console.log(`DB Error ${err}`);
+    console.log(`Error Retrieving Book, DB Error ${err}`);
   }
-
-  if (bookQuery.rows.length === 0)
-    return res.send("Error Retrieving Book").status(500);
-
-  const BOOK = bookQuery.rows[0];
+  if (!book) return res.send("Error Retrieving Book").status(500);
   try {
-    var reviewQuery = await DB.query(
-      `SELECT * FROM book_reviews
-     WHERE book_id = $1`,
-      [BOOK_ID],
-    );
+    var reviews = await databaseHandler.fetchBookReviews(book.id);
   } catch (err) {
-    console.log(`DB Error ${err}`);
+    console.log(`Error Retrieving Reviews, DB Error ${err}`);
   }
-
-  // Finding no reviews won't result in an error, none will be displayed.
-  const REVIEWS = reviewQuery.rows;
-
   return res.render("book_focus.ejs", {
-    book: BOOK,
-    user: user,
-    reviews: REVIEWS,
+    book: book,
+    user: req.user,
+    reviews: reviews,
   });
 });
 
@@ -245,10 +199,10 @@ APP.post("/add_review", async (req, res) => {
   if (!req.user || !req.body) return res.send("Server Error").status(500);
 
   try {
-    var userQuery = await DB.query(
+    var userQuery = await databaseHandler.database.query(
       `SELECT * FROM users
      WHERE email = $1`,
-      [req.user.email],
+      [req.user.email]
     );
   } catch (err) {
     console.log(err);
@@ -258,31 +212,19 @@ APP.post("/add_review", async (req, res) => {
     return res.send("Error retrieving Profile").status(500);
 
   try {
-    await DB.query(
-      `INSERT INTO book_reviews (
-       review_title,
-       reviewer_name,
-       review_date,
-       review_text,
-       user_id,
-       review_rating,
-       book_id
-     )
-     VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-      [
-        req.body.title,
-        userQuery.rows[0].name,
-        today(),
-        req.body.review,
-        userQuery.rows[0].id,
-        req.body.rating,
-        req.body.book_id,
-      ],
-    );
+    const REVIEW_INFO = [
+      req.body.title,
+      userQuery.rows[0].name,
+      today(),
+      req.body.review,
+      userQuery.rows[0].id,
+      req.body.rating,
+      req.body.book_id,
+    ];
+    await databaseHandler.addBookReview(REVIEW_INFO);
   } catch (err) {
     console.log(`DB Error ${err}`);
   }
-
   res.redirect(`/book_focus?book_id=${req.body.book_id}`);
 });
 
@@ -290,7 +232,7 @@ APP.get("/cart", async (req, res) => {
   if (req.isAuthenticated() === false) return res.render("login.ejs");
 
   try {
-    var cartItems = await getCart(req.user.id)
+    var cartItems = await getCart(req.user.id);
   } catch (err) {
     return res.send(`Error Retrieving Cart ${err}`).status(500);
   }
@@ -298,11 +240,11 @@ APP.get("/cart", async (req, res) => {
   return res.render("cart.ejs", { user: req.user, cart: cartItems });
 });
 
-async function getCart(user_id){
-  const CART_QUERY = await DB.query(
+async function getCart(user_id) {
+  const CART_QUERY = await databaseHandler.database.query(
     `SELECT * FROM carts
      WHERE user_id = $1`,
-    [user_id],
+    [user_id]
   );
 
   return CART_QUERY.rows;
@@ -310,10 +252,10 @@ async function getCart(user_id){
 
 APP.post("/add_cart", async (req, res) => {
   try {
-    const USER_QUERY = await DB.query(
+    const USER_QUERY = await databaseHandler.database.query(
       `SELECT * FROM users
      WHERE email = $1`,
-      [req.body.user_email],
+      [req.body.user_email]
     );
     var userId = USER_QUERY.rows[0].id;
   } catch (err) {
@@ -324,13 +266,13 @@ APP.post("/add_cart", async (req, res) => {
            of books remaining in the books table is updated, so will this table
            need to be updated. */
   try {
-    var bookQuery = await DB.query(
+    var bookQuery = await databaseHandler.database.query(
       `SELECT * FROM public.carts
        WHERE
          book_id = $1
        AND
          user_id = $2`,
-      [req.body.book_id, userId],
+      [req.body.book_id, userId]
     );
     console.log(bookQuery.rows);
   } catch (err) {
@@ -339,7 +281,7 @@ APP.post("/add_cart", async (req, res) => {
 
   if (bookQuery.rows.length > 0) {
     try {
-      await DB.query(
+      await databaseHandler.database.query(
         `UPDATE public.carts
          SET
            amount = $1
@@ -347,7 +289,7 @@ APP.post("/add_cart", async (req, res) => {
            book_id = $2
          AND
            user_id = $3`,
-        [bookQuery.rows[0].amount + 1, req.body.book_id, userId],
+        [bookQuery.rows[0].amount + 1, req.body.book_id, userId]
       );
     } catch {
       return res.send(`Error Adding Item to Cart: ${err}`).status(500);
@@ -358,7 +300,7 @@ APP.post("/add_cart", async (req, res) => {
   }
 
   try {
-    await DB.query(
+    await databaseHandler.database.query(
       `INSERT INTO public.carts
      (
        book_id,
@@ -376,7 +318,7 @@ APP.post("/add_cart", async (req, res) => {
         req.body.book_price,
         req.body.book_remaining,
         1,
-      ],
+      ]
     );
   } catch (err) {
     return res.send(`Error Adding Item to Cart: ${err}`).status(500);
@@ -402,7 +344,7 @@ APP.post(
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
-  }),
+  })
 );
 
 APP.get("/register", (_req, res) => {
@@ -417,10 +359,10 @@ APP.post("/register", async (req, res) => {
   const NAME = req.body.name;
 
   try {
-    var checkResult = await DB.query(
+    var checkResult = await databaseHandler.database.query(
       `SELECT * FROM users
      WHERE email = $1`,
-      [EMAIL],
+      [EMAIL]
     );
   } catch (err) {
     console.log(`DB Error ${err}`);
@@ -431,10 +373,10 @@ APP.post("/register", async (req, res) => {
   try {
     const HASH = await bcrypt.hash(PASSWORD, SALT_ROUNDS);
     try {
-      var newUserQuery = await DB.query(
+      var newUserQuery = await databaseHandler.database.query(
         `INSERT INTO users (email, password, name)
        VALUES ($1, $2, $3) RETURNING *`,
-        [EMAIL, HASH, NAME],
+        [EMAIL, HASH, NAME]
       );
     } catch (err) {
       console.log(`Error Adding New User to the DB: ${err}`);
@@ -448,11 +390,11 @@ APP.post("/register", async (req, res) => {
     const USER_ROLE_NAME = "user";
 
     try {
-      var userQuery = await DB.query(
+      var userQuery = await databaseHandler.database.query(
         `INSERT INTO user_roles (user_id, role_id, email, role)
        VALUES ($1, $2, $3, $4)
        RETURNING *`,
-        [NEW_USER.id, USER_ROLE_ID, EMAIL, USER_ROLE_NAME],
+        [NEW_USER.id, USER_ROLE_ID, EMAIL, USER_ROLE_NAME]
       );
     } catch (err) {
       console.log(`DB Error: ${err}`);
@@ -475,9 +417,10 @@ passport.use(
   "local",
   new Strategy(async function verify(username, password, callback) {
     try {
-      var result = await DB.query("SELECT * FROM users WHERE email = $1 ", [
-        username,
-      ]);
+      var result = await databaseHandler.database.query(
+        "SELECT * FROM users WHERE email = $1 ",
+        [username]
+      );
     } catch (err) {
       console.log(`DB Error ${err}`);
     }
@@ -493,7 +436,7 @@ passport.use(
       if (!VALID) return callback(null, false);
 
       try {
-        var userQuery = await DB.query(
+        var userQuery = await databaseHandler.database.query(
           `SELECT email, role,
              CASE
                WHEN role = 'admin' THEN 'admin'
@@ -508,7 +451,7 @@ passport.use(
                ELSE 3
              END
            LIMIT 1;`,
-          [USER.id],
+          [USER.id]
         );
       } catch (err) {
         console.log(`DB Error ${err}`);
@@ -517,8 +460,8 @@ passport.use(
         id: USER.id,
         email: userQuery.rows[0].email,
         role: userQuery.rows[0].role,
-        cart: await getCart()
-      }
+        cart: await getCart(),
+      };
 
       return callback(null, USER_DATA);
     } catch (err) {
@@ -526,7 +469,7 @@ passport.use(
 
       return callback(err);
     }
-  }),
+  })
 );
 
 passport.serializeUser((user, callback) => {
---
+++ JS/Challenges/Book Catalogue/models/databasehandler.js
@@ -0,0 +1,73 @@
+import pg from "pg";
+
+export default class DatabaseHandler {
+  constructor(clientInfo) {
+    this.database = new pg.Client(clientInfo);
+    this.database.connect();
+  }
+
+  async fetchBooks() {
+    const QUERY = await this.database.query("SELECT * FROM books");
+    return QUERY.rows;
+  }
+
+  async fetchBook(bookInfo) {
+    if (bookInfo.category) {
+      return await this.fetchBookByCategory(category);
+    }
+    if (bookInfo.id) {
+      return await this.fetchBookByID(bookInfo.id);
+    }
+  }
+
+  async fetchBookByCategory() {}
+
+  async fetchBookByID(id) {
+    const QUERY = await this.database.query("SELECT * FROM books WHERE id = $1",
+        [id]
+    );
+    return QUERY.rows[0];
+  }
+
+  async addBook(bookInfo) {
+    this.database.query(
+      `INSERT INTO books (
+         title,
+         author,
+         category,
+         publish_year,
+         abstract,
+         cover_id,
+         quantity,
+         price
+         )
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+      bookInfo
+    );
+  }
+
+  async fetchBookReviews(id) {
+    const QUERY = await this.database.query(
+      `SELECT * FROM book_reviews
+       WHERE book_id = $1`,
+      [BOOK_ID]
+    );
+    return QUERY.rows;
+  }
+
+  async addBookReview(reviewInfo) {
+    await this.database.query(
+      `INSERT INTO book_reviews (
+         review_title,
+         reviewer_name,
+         review_date,
+         review_text,
+         user_id,
+         review_rating,
+         book_id
+       )
+       VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+      reviewInfo
+    );
+  }
+}
