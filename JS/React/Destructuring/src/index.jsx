1cec5ded7dc4eae0033072cb8d3f37aed52718e4
Sins_621 <sinsmailza@gmail.com>
2025-03-25 15:29:38 +0200

    add/data-destructuring-example

---
+++ JS/React/Destructuring/src/index.jsx
@@ -0,0 +1,42 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice.js";
+
+const [honda, tesla] = cars;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+  coloursByPopularity: [teslaTopColour],
+} = tesla;
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+  coloursByPopularity: [hondaTopColour],
+} = honda;
+
+ReactDOM.render(
+  <table>
+    <tbody>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </tbody>
+  </table>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
